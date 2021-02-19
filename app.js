9ee0038d6d0656ac8d1df66a6dc2011efeceb23b
Jose <gauranajose@gmail.com>
2021-02-19 17:06:54 +0800

    3rd commit

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const elements = document.querySelectorAll('ul li');
 
-console.log(elements);
-
 const toggleHandler = event => {
     event.stopPropagation();
     const element = event.currentTarget.firstElementChild;
     element.nextElementSibling.classList.toggle('hidden');
     element.firstElementChild.classList.toggle('active');
     element.lastElementChild.classList.toggle('active-img');
+    document.body.style.height = document.documentElement.scrollHeight + 'px';
 }
 
 for(const el of elements) {
